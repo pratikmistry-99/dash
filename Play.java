1223a10ca3c2f29e12f43393692b395544fe2861
Rithik Sowdermett <rsowd046@uottawa.ca>
2018-12-01 13:06:35 -0500

    play class added

---
+++ Play.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class Play extends JFrame{
+	//JFrame frame;
+	//Panel panel;
+	
+	public Play (JPanel panel){
+		
+		//frame = fr;/*
+		this.setPreferredSize(new Dimension(700,400));
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		panel.setBackground(Color.green);
+		
+	}
+	
+	public static void main (String[] args){
+		//new Play(new JFrame("DASH"));
+	}
+	
+}

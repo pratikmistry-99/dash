0a64db59b548d9a59af2385e7bd3c729aaf2f15f
pratikmistry-99 <pmist017@uottawa.ca>
2018-12-01 12:06:02 -0500

    added welcome screen

---
+++ WelcomeScreen.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class WelcomeScreen extends JFrame{
+
+    public WelcomeScreen() {
+        super("hello");
+        this.setPreferredSize(new Dimension(700,400));
+
+        JPanel panel = new JPanel(new GridLayout(4,4,4,4));
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JLabel dash = (new JLabel("DASH!",SwingConstants.CENTER));
+        dash.setFont(new Font("Courier",Font.BOLD,75));
+        panel.add(dash);
+        panel.setVisible(true);
+
+        JButton btn = (new JButton("PLAY"));
+        btn.setPreferredSize(new Dimension(2,4));
+        panel.add(btn);
+        this.pack();
+        this.add(panel);
+        this.setVisible(true);
+
+
+
+    }
+
+    public static void main(final String[] args) {
+        new WelcomeScreen();
+    }
+
+}
